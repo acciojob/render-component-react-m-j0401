f9af6ded143485f31435f981e66ee6d104c58912
Mehul Jain <mehul.jain2206@gmail.com>
2026-03-23 13:17:52 +0530

    render2

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
-  [str,setStr]=useState("")
+ let [str,setStr]=useState("")
   return (
     <div id="main">
-      <p>setStr(I am learning React. My life is getting better.)</p>
+      <button onClick={()=>setStr("I am learning React. My life is getting better.")}>Show</button>
+      <p>{str}</p>
     </div>
   )
 }
