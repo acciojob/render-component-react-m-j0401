0c700e320d5e2b131a811e61647bfe58157894cd
Mehul Jain <mehul.jain2206@gmail.com>
2026-03-23 12:49:48 +0530

    render

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,11 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  [str,setStr]=useState("")
   return (
-    <div id="main"></div>
+    <div id="main">
+      <p>setStr(I am learning React. My life is getting better.)</p>
+    </div>
   )
 }
 
